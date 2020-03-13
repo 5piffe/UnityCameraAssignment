021d8c3d04e96a8b29c6796fd0fc02cd0fe6b654
5piffe <richard.nordstrom@futuregames.nu>
2020-03-13 14:25:45 +0100

    Added unity project

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    #region binding IDs
+    private const string mouseXInput = "Mouse X";
+    private const string mouseYInput = "Mouse Y";
+    private const string zoomInput = "Mouse ScrollWheel";
+    #endregion binding IDs
+
+    [Header("Camera Preferences")]
+    [Tooltip("Target to follow")] public GameObject cameraTarget;
+
+    [SerializeField] private float horizontalSensitivity = 1.25f;
+    [SerializeField] private float verticalSensitivity = 0.75f;
+    [Tooltip("Invert looking up/down.")]
+    [SerializeField] private bool invertY = false;
+
+    [SerializeField, Range(1, 10)] private float maxZoomOut = 6f;
+    [SerializeField] private float zoomSensitivity = 3f;
+
+    [Tooltip("Cap viewing angle on the Y axis.")]
+    [SerializeField, Range(0f, 90f)] private float maxLookDownAngle = 70f;
+    [SerializeField, Range(0f, 90f)] private float maxLookUpAngle = 45f;
+        
+    [Header("Smoothing (lower = smoother camera).")]
+    [SerializeField, Range(2f, 40f)] private float smoothness = 12f;
+    [SerializeField] private bool useSmoothing = true;
+
+    private float cameraOffset;
+    private float mouseX;
+    private float mouseY;
+    private float zoom;
+
+	private void Awake()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        cameraOffset = Vector3.Distance(transform.position, cameraTarget.transform.position);
+    }
+
+    private void Update()
+    {
+        CamerInput();
+    }
+
+    private void LateUpdate()
+    {
+        CameraUpdate();
+        ObstacleCheck();
+    }
+
+    private void CamerInput()
+    {
+		zoom += Input.GetAxis(zoomInput) * zoomSensitivity;
+		zoom = Mathf.Clamp(zoom, -maxZoomOut, cameraOffset);
+
+		mouseX = Input.GetAxis(mouseXInput) * horizontalSensitivity;
+		mouseY += (!invertY) ? Input.GetAxis(mouseYInput) * verticalSensitivity : Input.GetAxis(mouseYInput) * -verticalSensitivity;
+		mouseY = Mathf.Clamp(mouseY, -maxLookDownAngle, maxLookUpAngle);
+
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("setting camera behind player");
+            //transform.rotation = Quaternion.Euler(cameraTarget.transform.rotation.x, 0f, 0f);
+        }
+
+    }
+
+    private void CameraUpdate()
+    {
+        transform.eulerAngles = new Vector3(-mouseY, transform.eulerAngles.y + mouseX, 0);
+
+        transform.position = (!useSmoothing) ? cameraTarget.transform.position - (transform.forward * (cameraOffset - zoom)) :
+            Vector3.Slerp(transform.position, cameraTarget.transform.position - (transform.forward * (cameraOffset - zoom)), smoothness * Time.fixedDeltaTime);
+    }
+
+    void ObstacleCheck()
+    {
+        float currentCameraOffset = Vector3.Distance(cameraTarget.transform.position, transform.position);
+        float sphereRadius = 0.12f;
+
+        if (Physics.SphereCast(cameraTarget.transform.position, sphereRadius,transform.position - cameraTarget.transform.position, out RaycastHit hit, currentCameraOffset))
+        {
+            transform.position = hit.point;
+        }
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerController : MonoBehaviour
+{
+	#region binding IDs
+	private const string horizontal = "Horizontal";
+	private const string vertical = "Vertical";
+	#endregion binding IDs
+	
+	[Tooltip("The camera following this target")]
+	public Transform theCamera;
+	public GameObject playerModel;
+
+	[Tooltip("How fast the player rotates towards a new direction")]
+	[SerializeField] private float turnSpeed = 15f;
+	[SerializeField] private float walkSpeed = 6f;
+
+	[Tooltip("PlayerModel transparency if too close to the camera")]
+	[SerializeField] private float distanceToTransparent = 1.5f;
+	[SerializeField, Range(0.01f, 1f)] private float transparency = 0.25f;
+
+	private CharacterController characterController;
+	private Component[] meshRenderers;
+	private Vector3 moveDirection;
+
+	private void Awake()
+	{
+		characterController = GetComponent<CharacterController>();
+		meshRenderers = GetComponentsInChildren<MeshRenderer>();
+	}
+
+	private void Update()
+	{
+		PlayerMovement();
+		ChangeColorAlpha();
+	}
+
+	void PlayerMovement()
+	{
+		moveDirection = transform.forward * Input.GetAxis(vertical) + transform.right * Input.GetAxis(horizontal);
+		moveDirection = moveDirection.normalized * walkSpeed;
+		characterController.Move(moveDirection * Time.deltaTime);
+
+		if (Input.GetAxis(horizontal) != 0 || Input.GetAxis(vertical) != 0)
+		{
+			transform.rotation = Quaternion.Euler(0f, theCamera.rotation.eulerAngles.y, 0f);
+			Quaternion newRotation = Quaternion.LookRotation(new Vector3(moveDirection.x, 0f, moveDirection.z));
+			playerModel.transform.rotation = Quaternion.Slerp(playerModel.transform.rotation, newRotation, turnSpeed * Time.deltaTime);
+		}
+	}
+
+	private void ChangeColorAlpha()
+	{
+		
+		if (Vector3.Distance(theCamera.transform.position, transform.position) < distanceToTransparent)
+		{
+			foreach (MeshRenderer meshRenderer in meshRenderers)
+			{
+				Color newColor = meshRenderer.material.color;
+				newColor.a = transparency;
+				meshRenderer.material.color = newColor;
+			}
+		}
+		else
+		{
+			foreach (MeshRenderer meshRenderer in meshRenderers)
+			{
+				Color newColor = meshRenderer.material.color;
+				newColor.a = 1f;
+				meshRenderer.material.color = newColor;
+			}
+		}
+	}
+}
